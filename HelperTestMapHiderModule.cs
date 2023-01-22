691cec80033fecb5b3850f8487ddbd6c6614299e
Kalobi <46748261+Kalobi@users.noreply.github.com>
2023-01-22 22:58:24 +0100

    First version

---
+++ HelperTestMapHiderModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Celeste.Mod.HelperTestMapHider {
+    public class HelperTestMapHiderModule : EverestModule
+    {
+
+        public static List<string> helpers = new List<string> {"BounceHelper", "CustomPoints", "HonlyHelper", "JackalHelper"};
+        public override void Load()
+        {
+            On.Celeste.Mod.Everest.Content.TryAdd += OnTryAdd;
+        }
+
+        public override void Unload()
+        {
+            On.Celeste.Mod.Everest.Content.TryAdd -= OnTryAdd;
+        }
+
+        private static bool OnTryAdd(On.Celeste.Mod.Everest.Content.orig_TryAdd orig, string path, ModAsset metadata)
+        {
+            var cleanPath = path.Replace('\\', '/');
+
+            if (helpers.Contains(metadata.Source.Name) && cleanPath.StartsWith("/Maps/"))
+            {
+                return false;
+            }
+
+            return orig(path, metadata);
+        }
+
+    }
+}
